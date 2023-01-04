d2fa3fc05ec3c7aa354c5cbaa1ad88495707a568
jb3rndt <berndtjannis@gmail.com>
2023-01-04 23:20:08 +0100

    Heavily strip down _TabSwitchingViewState and make sliding between tabs possible

--- lib/components/persistent_tab_view_scaffold.dart
+++ lib/components/persistent_tab_view_scaffold.dart
@@ -203,215 +203,74 @@ class _TabSwitchingView extends StatefulWidget {
   _TabSwitchingViewState createState() => _TabSwitchingViewState();
 }
 
-class _TabSwitchingViewState extends State<_TabSwitchingView>
-    with TickerProviderStateMixin {
-  final List<bool> shouldBuildTab = <bool>[];
-  final List<FocusScopeNode> tabFocusNodes = <FocusScopeNode>[];
-  final List<FocusScopeNode> discardedNodes = <FocusScopeNode>[];
-  late List<AnimationController?> _animationControllers;
-  late List<Animation<double>?> _animations;
-  int? _tabCount;
-  late int _currentTabIndex;
-  late int _previousTabIndex;
-  bool? _showAnimation;
-  double? _animationValue;
-  Curve? _animationCurve;
-  Key? key;
+class _TabSwitchingViewState extends State<_TabSwitchingView> {
+  late PageController _pageController;
 
   @override
   void initState() {
     super.initState();
-    shouldBuildTab.addAll(List<bool>.filled(widget.tabCount!, false));
-    _currentTabIndex = widget.currentTabIndex;
-    _previousTabIndex = widget.currentTabIndex;
-    _tabCount = widget.tabCount;
-    _showAnimation = widget.screenTransitionAnimation!.animateTabTransition;
-
-    if (!widget.stateManagement!) {
-      key = UniqueKey();
-    }
-
-    _initAnimationControllers();
-  }
-
-  _initAnimationControllers() {
-    if (widget.screenTransitionAnimation!.animateTabTransition) {
-      _animationControllers =
-          List<AnimationController?>.filled(widget.tabCount!, null);
-      _animations = List<Animation<double>?>.filled(widget.tabCount!, null);
-      _animationCurve = widget.screenTransitionAnimation!.curve;
-      for (int i = 0; i < widget.tabCount!; ++i) {
-        _animationControllers[i] = AnimationController(
-            vsync: this, duration: widget.screenTransitionAnimation!.duration);
-        _animations[i] = Tween(begin: 0.0, end: 0.0)
-            .chain(CurveTween(curve: widget.screenTransitionAnimation!.curve))
-            .animate(_animationControllers[i]!);
-      }
-
-      for (int i = 0; i < widget.tabCount!; ++i) {
-        _animationControllers[i]!.addListener(() {
-          if (_animationControllers[i]!.isCompleted) {
-            _previousTabIndex = _currentTabIndex;
-            setState(() {
-              if (!widget.stateManagement!) {
-                key = UniqueKey();
-              }
-            });
-          }
-        });
-      }
-    }
-  }
-
-  void _focusActiveTab() {
-    if (widget.screenTransitionAnimation!.animateTabTransition)
-      _startAnimation();
-    if (tabFocusNodes.length != widget.tabCount) {
-      if (tabFocusNodes.length > widget.tabCount!) {
-        discardedNodes.addAll(tabFocusNodes.sublist(widget.tabCount!));
-        tabFocusNodes.removeRange(widget.tabCount!, tabFocusNodes.length);
-      } else {
-        tabFocusNodes.addAll(
-          List<FocusScopeNode>.generate(
-            widget.tabCount! - tabFocusNodes.length,
-            (int index) => FocusScopeNode(
-                debugLabel:
-                    '$CupertinoTabScaffold Tab ${index + tabFocusNodes.length}'),
-          ),
-        );
-      }
-    }
-    FocusScope.of(context).setFirstFocus(tabFocusNodes[_currentTabIndex]);
-  }
-
-  /// Starts the animation from one tab to another by shifting the previous
-  /// tab to the left/right and making the new tab appear from left/right.
-  /// This method must only be called once when [_previousTabIndex]
-  /// and [_currentTabIndex] change.
-  void _startAnimation() {
-    if (_previousTabIndex == _currentTabIndex) return;
-    _animationControllers[_previousTabIndex]!.reset();
-    _animations[_previousTabIndex] = Tween(
-            begin: 0.0,
-            end: (_previousTabIndex > _currentTabIndex)
-                ? _animationValue
-                : -_animationValue!)
-        .chain(CurveTween(curve: widget.screenTransitionAnimation!.curve))
-        .animate(_animationControllers[_previousTabIndex]!);
-
-    _animationControllers[_currentTabIndex]!.reset();
-    _animations[_currentTabIndex] = Tween(
-      begin: (_previousTabIndex > _currentTabIndex)
-          ? -_animationValue!
-          : _animationValue!,
-      end: 0.0,
-    )
-        .chain(CurveTween(curve: widget.screenTransitionAnimation!.curve))
-        .animate(_animationControllers[_currentTabIndex]!);
-
-    _animationControllers[_previousTabIndex]!.forward();
-    _animationControllers[_currentTabIndex]!.forward();
-    return;
-  }
-
-  Widget _buildScreens() {
-    return Stack(
-      fit: StackFit.expand,
-      children: List<Widget>.generate(widget.tabCount!, (int index) {
-        final bool active = index == _currentTabIndex ||
-            (widget.screenTransitionAnimation!.animateTabTransition &&
-                index == _previousTabIndex);
-        shouldBuildTab[index] = active || shouldBuildTab[index];
-
-        return Offstage(
-          offstage: !active,
-          child: TickerMode(
-            enabled: active,
-            child: FocusScope(
-              node: tabFocusNodes[index],
-              child: Builder(
-                builder: (BuildContext context) {
-                  return shouldBuildTab[index]
-                      ? (widget.screenTransitionAnimation!.animateTabTransition
-                          ? AnimatedBuilder(
-                              animation: _animations[index]!,
-                              builder: (context, child) => Transform.translate(
-                                offset: Offset(_animations[index]!.value, 0),
-                                child: widget.tabBuilder(context, index),
-                              ),
-                            )
-                          : widget.tabBuilder(context, index))
-                      : Container();
-                },
-              ),
-            ),
-          ),
-        );
-      }),
-    );
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    _focusActiveTab();
+    _pageController = PageController(initialPage: widget.currentTabIndex);
   }
 
   @override
   void didUpdateWidget(_TabSwitchingView oldWidget) {
     super.didUpdateWidget(oldWidget);
-    final int lengthDiff = widget.tabCount! - shouldBuildTab.length;
-    if (lengthDiff > 0) {
-      shouldBuildTab.addAll(List<bool>.filled(lengthDiff, false));
-    } else if (lengthDiff < 0) {
-      shouldBuildTab.removeRange(widget.tabCount!, shouldBuildTab.length);
-    }
     if (widget.currentTabIndex != oldWidget.currentTabIndex) {
-      _currentTabIndex = widget.currentTabIndex;
-      _previousTabIndex = oldWidget.currentTabIndex;
-      _focusActiveTab();
+      _pageController.animateToPage(
+        widget.currentTabIndex,
+        duration:
+            widget.screenTransitionAnimation?.duration ?? Duration(seconds: 0),
+        curve: widget.screenTransitionAnimation?.curve ?? Curves.easeInOut,
+      );
     }
   }
 
   @override
   void dispose() {
-    for (FocusScopeNode focusScopeNode in tabFocusNodes) {
-      focusScopeNode.dispose();
-    }
-    for (FocusScopeNode focusScopeNode in discardedNodes) {
-      focusScopeNode.dispose();
-    }
-    if (widget.screenTransitionAnimation!.animateTabTransition) {
-      for (AnimationController? controller in _animationControllers) {
-        controller!.dispose();
-      }
-    }
+    _pageController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    _animationValue = MediaQuery.of(context).size.width;
-    if (_tabCount != widget.tabCount) {
-      _tabCount = widget.tabCount;
-      _initAnimationControllers();
-    }
-    if (widget.screenTransitionAnimation!.animateTabTransition &&
-            _animationControllers.first!.duration !=
-                widget.screenTransitionAnimation!.duration ||
-        _animationCurve != widget.screenTransitionAnimation!.curve) {
-      _initAnimationControllers();
-    }
-    if (_showAnimation !=
-        widget.screenTransitionAnimation!.animateTabTransition) {
-      _showAnimation = widget.screenTransitionAnimation!.animateTabTransition;
-      key = UniqueKey();
-    }
-    return widget.stateManagement!
-        ? _buildScreens()
-        : KeyedSubtree(
-            key: key,
-            child: _buildScreens(),
-          );
+    return PageView(
+      controller: _pageController,
+      children: List.generate(widget.tabCount!, (int index) {
+        return FocusScope(
+          node: FocusScopeNode(),
+          child: KeepAlivePage(
+            child: widget.tabBuilder(context, index),
+          ),
+        );
+      }),
+      onPageChanged: (i) {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+    );
   }
+}
+
+class KeepAlivePage extends StatefulWidget {
+  KeepAlivePage({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  _KeepAlivePageState createState() => _KeepAlivePageState();
+}
+
+class _KeepAlivePageState extends State<KeepAlivePage>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+
+    return widget.child;
+  }
+
+  @override
+  bool get wantKeepAlive => true;
 }
