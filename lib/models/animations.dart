d215ab01f487e8bee83625df5b67ef2fafba0964
jb3rndt <berndtjannis@gmail.com>
2023-02-27 22:14:42 +0100

    Remove unused field _animateTabTransition

--- lib/models/animations.dart
+++ lib/models/animations.dart
@@ -4,18 +4,14 @@ class ScreenTransitionAnimation {
   const ScreenTransitionAnimation({
     this.duration = const Duration(milliseconds: 200),
     this.curve = Curves.ease,
-  }) : _animateTabTransition = true;
+  });
 
   const ScreenTransitionAnimation.none()
-      : _animateTabTransition = false,
-        duration = Duration.zero,
+      : duration = Duration.zero,
         curve = Curves.linear;
 
-  final bool _animateTabTransition;
   final Duration duration;
   final Curve curve;
-
-  bool get animateTabTransition => _animateTabTransition;
 }
 
 class ItemAnimation {
