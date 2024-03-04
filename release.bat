8749271e6412ac763e423bd2e00763fedbe61499
jb3rndt <berndtjannis@gmail.com>
2024-03-04 21:30:59 +0100

    Also commit pubspec.lock

--- release.bat
+++ release.bat
@@ -19,7 +19,7 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`cider version`) DO (
 @echo Changes to release:
 call cider describe
 call cider release
-call git commit pubspec.yaml CHANGELOG.md example/pubspec.lock -m "Version %new_ver%"
+call git commit pubspec.yaml pubspec.lock CHANGELOG.md example/pubspec.lock -m "Version %new_ver%"
 call git tag -a v%new_ver% -m "Version %new_ver%"
 call git push
 call git push --tags
